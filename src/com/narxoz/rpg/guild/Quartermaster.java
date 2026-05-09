20c0aaabff6561f0cd02d9d34263738a374f6791
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-05-09 21:30:55 +0500

    add quartermaster mediated behavior

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -9,13 +9,28 @@ public class Quartermaster extends GuildMember {
         super(name, mediator);
     }
 
-    public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
-        getMediator().dispatch(topic, this, payload);
+    public void requestSupplies(String payload) {
+        getMediator().dispatch(GuildHall.TOPIC_SUPPLIES, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = senderName(from);
+        if (GuildHall.TOPIC_ORDERS.equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] equips the party for "
+                    + sender + "'s order: " + payload);
+        } else if (GuildHall.TOPIC_SCOUTING.equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] checks ropes, maps, and torches after "
+                    + sender + "'s route report: " + payload);
+        } else if (GuildHall.TOPIC_URGENT.equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] opens the emergency armory: "
+                    + payload);
+        } else if (GuildHall.TOPIC_REWARDS.equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] prepares the reward ledger: "
+                    + payload);
+        } else {
+            System.out.println("[Quartermaster " + getName() + "] logs " + topic + " from "
+                    + sender + ": " + payload);
+        }
     }
 }
