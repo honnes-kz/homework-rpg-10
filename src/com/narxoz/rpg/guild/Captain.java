65b89f30e68b733622c9cc5b4bf9efe828352025
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-05-09 16:02:51 +0500

    add captain mediated behavior

--- src/com/narxoz/rpg/guild/Captain.java
+++ src/com/narxoz/rpg/guild/Captain.java
@@ -9,13 +9,34 @@ public class Captain extends GuildMember {
         super(name, mediator);
     }
 
-    public void issueOrder(String topic, String payload) {
-        // TODO: send a command message through the mediator.
-        getMediator().dispatch(topic, this, payload);
+    public void issueOrder(String payload) {
+        getMediator().dispatch(GuildHall.TOPIC_ORDERS, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = senderName(from);
+        if (GuildHall.TOPIC_SCOUTING.equals(topic)) {
+            System.out.println("[Captain " + getName() + "] updates the battle plan from "
+                    + sender + "'s scouting report: " + payload);
+        } else if (GuildHall.TOPIC_SUPPLIES.equals(topic)) {
+            System.out.println("[Captain " + getName() + "] approves the supply plan from "
+                    + sender + ": " + payload);
+        } else if (GuildHall.TOPIC_HEALING.equals(topic)) {
+            System.out.println("[Captain " + getName() + "] schedules recovery support from "
+                    + sender + ": " + payload);
+        } else if (GuildHall.TOPIC_URGENT.equals(topic)) {
+            System.out.println("[Captain " + getName() + "] calls for immediate readiness: "
+                    + payload);
+        } else if (GuildHall.TOPIC_REWARDS.equals(topic)) {
+            System.out.println("[Captain " + getName() + "] weighs the reward against the risk: "
+                    + payload);
+        } else if (GuildHall.TOPIC_LORE.equals(topic)) {
+            System.out.println("[Captain " + getName() + "] adds historical context to the briefing: "
+                    + payload);
+        } else {
+            System.out.println("[Captain " + getName() + "] receives " + topic + " from "
+                    + sender + ": " + payload);
+        }
     }
 }
