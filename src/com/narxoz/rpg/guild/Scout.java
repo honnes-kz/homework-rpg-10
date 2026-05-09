4bb97521027e81d917fec40b17ebd076410e76e8
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-05-09 21:31:21 +0500

    add scout mediated behavior

--- src/com/narxoz/rpg/guild/Scout.java
+++ src/com/narxoz/rpg/guild/Scout.java
@@ -9,13 +9,28 @@ public class Scout extends GuildMember {
         super(name, mediator);
     }
 
-    public void reportRoute(String topic, String payload) {
-        // TODO: send a scouting message through the mediator.
-        getMediator().dispatch(topic, this, payload);
+    public void reportRoute(String payload) {
+        getMediator().dispatch(GuildHall.TOPIC_SCOUTING, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = senderName(from);
+        if (GuildHall.TOPIC_ORDERS.equals(topic)) {
+            System.out.println("[Scout " + getName() + "] marks a route for "
+                    + sender + "'s order: " + payload);
+        } else if (GuildHall.TOPIC_SUPPLIES.equals(topic)) {
+            System.out.println("[Scout " + getName() + "] adjusts the field kit request: "
+                    + payload);
+        } else if (GuildHall.TOPIC_URGENT.equals(topic)) {
+            System.out.println("[Scout " + getName() + "] sends runners to verify the threat: "
+                    + payload);
+        } else if (GuildHall.TOPIC_LORE.equals(topic)) {
+            System.out.println("[Scout " + getName() + "] adds the lore warning to the route map: "
+                    + payload);
+        } else {
+            System.out.println("[Scout " + getName() + "] notes " + topic + " from "
+                    + sender + ": " + payload);
+        }
     }
 }
