4806b9967dea3dadc80cfbd1087ef97f739d5c2e
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-05-09 21:57:26 +0500

    add healer mediated behavior

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -9,13 +9,28 @@ public class Healer extends GuildMember {
         super(name, mediator);
     }
 
-    public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
-        getMediator().dispatch(topic, this, payload);
+    public void prepareAid(String payload) {
+        getMediator().dispatch(GuildHall.TOPIC_HEALING, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = senderName(from);
+        if (GuildHall.TOPIC_ORDERS.equals(topic)) {
+            System.out.println("[Healer " + getName() + "] prepares triage for "
+                    + sender + "'s order: " + payload);
+        } else if (GuildHall.TOPIC_SCOUTING.equals(topic)) {
+            System.out.println("[Healer " + getName() + "] chooses antidotes for the route: "
+                    + payload);
+        } else if (GuildHall.TOPIC_SUPPLIES.equals(topic)) {
+            System.out.println("[Healer " + getName() + "] reserves potions and bandages: "
+                    + payload);
+        } else if (GuildHall.TOPIC_URGENT.equals(topic)) {
+            System.out.println("[Healer " + getName() + "] readies emergency healing circles: "
+                    + payload);
+        } else {
+            System.out.println("[Healer " + getName() + "] records " + topic + " from "
+                    + sender + ": " + payload);
+        }
     }
 }
